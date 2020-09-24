4831b44c6df89d4f30508ed56752058188c1b0b7
Michelle Aber <aberma@dukes.jmu.edu>
2020-09-23 21:28:04 -0400

    basic navbar+header

---
+++ homepage.js
@@ -0,0 +1,23 @@
+// const slides = document.querySelectorAll('.slide');
+// let i = 0;
+
+// function forward() {
+//   if (i < slides.length - 1) {
+//     slides[i].classList.remove('current');
+//     slides[i].classList.add('past');
+//     i += 1;
+//     slides[i].classList.add('current');
+//   }
+// }
+
+// function backward() {
+//   if (i > 0) {
+//     slides[i].classList.remove('current');
+//     i -= 1;
+//     slides[i].classList.remove('past');
+//     slides[i].classList.add('current');
+//   }
+// }
+
+// document.getElementById('left').addEventListener('click', backward);
+// document.getElementById('right').addEventListener('click', forward);
