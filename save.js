be7220f6cdc3924f01341f94a7ab331fd85579cb
Michelle Aber <aberma@dukes.jmu.edu>
2020-10-15 23:48:31 -0400

    added fav icon and attempted to store input form data into cookies

---
+++ save.js
@@ -0,0 +1,18 @@
+const submitButton = document.getElementById('submit-button');
+const name = document.getElementById('recipe-name');
+const value = name.value();
+
+submitButton.addEventListener('click', () => {
+   setCookie(name, value, 30);
+});
+
+function setCookie(name, value, numDays) {
+    let currentDate = new Date();
+    currentDate.setTime(currentDate.getTime() + (numDays*24*60*60*1000));
+    let expires = "expires="+ currentDate.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+ 
+  }
+
+// function getCookie(name) {
+// }
