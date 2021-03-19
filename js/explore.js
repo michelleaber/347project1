2388cf707bd2e827c87dc7728a3ac8ebdd27852a
Michelle Aber <aberma@dukes.jmu.edu>
2021-03-18 23:25:31 -0400

    changed explore page from card style to accordion style because less info on page

---
+++ js/explore.js
@@ -0,0 +1,18 @@
+const accordions = document.querySelectorAll('.section');
+console.log(accordions);
+for (let index = 0; index < accordions.length; index+=1) {
+    accordions[index].addEventListener('click', function () {
+        this.classList.toggle('open'); // why does it work with this but not accordions.item(index)?
+        // get the content
+        let content = this.nextElementSibling;
+        console.log(this.nextElementSibling);
+        // if clicked and the content is already open, then close it
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+
+        // if clicked and the content is currently closed, then open it
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
