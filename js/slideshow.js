f59d23204c6508463acd4a470ccb7b9ab0f2ae6e
Michelle Aber <aberma@dukes.jmu.edu>
2021-03-18 23:33:20 -0400

    fixed js console errors- separated slideshow and menu button into different javascript files

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,15 +1,3 @@
-const navigation = document.getElementById('navigation');
-const menuButton = document.getElementById('menu-button');
-
-menuButton.addEventListener('click', event => {
-  navigation.classList.toggle('open');
-  event.stopPropagation();
-});
-
-document.addEventListener('click', () => {
-  navigation.classList.remove('open');
-});
-
 const slides = document.querySelectorAll('.slide');
 let i = 0;
 
