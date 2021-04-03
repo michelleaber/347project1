afea019b720b921c83d2007ebfc74989d31b4ede
Michelle Aber <aberma@dukes.jmu.edu>
2021-04-03 16:01:08 -0400

    cleaned up code more + finished report + added Cook Book Title in Header to be clickable

--- js/explore.js
+++ js/explore.js
@@ -1,13 +1,13 @@
 const accordions = document.querySelectorAll('.card');
-for (let index = 0; index < accordions.length; index+=1) {
-    accordions[index].addEventListener('click', function () {
+for (let accordion of accordions) {
+    accordion.addEventListener('click', function () {
         this.classList.toggle('open');
         // get the content
         let content = this.nextElementSibling;
-        // if clicked and the content is already open, then close it
+        // if clicked and content is already open, then close it
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
-        // if clicked and the content is currently closed, then open it
+            // if clicked and content is currently closed, then open it
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         }
--- js/save.js
+++ js/save.js
@@ -1,24 +1,8 @@
-// const submitButton = document.getElementById('submit-button');
 const submitButtons = document.querySelectorAll('.button-submit');
-// const name = document.getElementById('recipe-name');
-// const value = name.value;
 
 for (let submitButton of submitButtons) {
-submitButton.addEventListener('click', () => {
-  //  setCookie(name, value, 30);
-   alert('Your input has been submitted. Thank you!');
-});
+  submitButton.addEventListener('click', () => {
+    alert('Your input has been submitted. Thank you!');
+  });
 }
 
-
-// function setCookie(name, value, numDays) {
-//     let currentDate = new Date();
-//     currentDate.setTime(currentDate.getTime() + (numDays*24*60*60*1000));
-//     let expires = "expires="+ currentDate.toUTCString();
-//     document.cookie = name + "=" + value + ";" + expires + ";path=/";
- 
-// }
-
-
-// function getCookie(name) {
-// }
